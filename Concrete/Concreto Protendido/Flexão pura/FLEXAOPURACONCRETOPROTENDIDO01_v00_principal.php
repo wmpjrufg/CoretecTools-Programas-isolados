630cd7394a041069f21a6773d622561c5dd9fd1a
carlosmagnosilvacarneiro <62339838+carlosmagnosilvacarneiro@users.noreply.github.com>
2020-07-14 22:14:03 -0300

    Atualização para concreto protendido
    Carlos Magno e Walfrido atualizaram a programação de concreto armado para concreto protendido

--- Concrete/Concreto Protendido/Flexão pura/FLEXAOPURACONCRETOPROTENDIDO01_v00_principal.php
+++ Concrete/Concreto Protendido/Flexão pura/FLEXAOPURACONCRETOPROTENDIDO01_v00_principal.php
@@ -3,18 +3,14 @@
 // UNIVERSIDADE FEDERAL DE CATALÃO (UFCat)
 // DEPARTAMENTO DE ENGENHARIA CIVIL & DEPARTAMENTO DE CIÊNCIAS DA COMPUTAÇÃO
 // Autores
-// Aleff Lucas Gonçalves Martins
-// Roberto Viegas Dutra
+// Carlos Magno Silva Carneiro
+// Walfrido Carvalho do Valle
 // Colaboradores
-// Luiz Eduardo Costa Mota Leite
-// Newton Peixoto
-// Pedro Henrique dos Santos Marques Vieira
-// Luanna Lopes Lobato
 // Wanderlei Malaquias Pereira Junior
 // ######################################################################
 
 // Nome:    Dimensionamento de seções retangulares submetidas à flexão pura
-// Versão:  FLEXAOPURACONCRETOARMADO01_v00_principal
+// Versão:  FLEXAOPURACONCRETOPROTENDIDO01_v00_principal
 // Notas:
 // 01-01-19 - A. L. G. Martins and R. V. Dutra finalizaram a versão beta
 // 01-01-20 - Prof. W. M. Pereira Junior and L. E. C. Mota Leite introduziram as verificações de erros e avisos
@@ -36,34 +32,33 @@
 //
 // Escalares
 //
-// fck           - Resistência característica à compressão do concreto (MPa)
-// dmax          - Diâmetro máximo do agregado graúdo                  (cm)
-// fyk           - Resistência característica à tração do aço          (MPa)
-// bw            - Largura da seção de concreto                        (cm)
-// h             - Altura da seção de concreto                         (cm)
-// cob           - Cobrimento das armaduras                            (cm)
-// phiestribo    - Diâmetro do estribo                                 (mm)
-// d             - Altura útil da peça (0.8.h <= d <= 0.9.h)           (cm)
-// md            - Momento fletor de cálculo                           (kN.m)
-// fcd           - Resistência de cálculo à compressão do concreto     (MPa)
-// fyd           - Resistência de cálculo à tração do aço              (MPa)
-// dlinha        - Altura útil complementar da peça                    (cm)
-// ac            - Área da seção transversal                           (cm2)
-// inercia       - Momento de inércia                                  (cm4)
-// ycg           - Baricentro da peça sem armadura no estádio I        (cm)
-// w             - Módulo resistente da peça sem armadura no estádio   (cm3)
+// fck           - Resistência característica à compressão do concreto         (MPa)
+// dmax          - Diâmetro máximo do agregado graúdo                          (cm)
+// fpyk          - Resistência característica à tração do aço de protensão     (MPa)
+// fptk          - Resistência mínima à ruptura por tração do aço de protenção (MPa)
+// bw            - Largura da seção de concreto                              (cm)
+// h             - Altura da seção de concreto                               (cm)
+// cob           - Cobrimento das armaduras                                  (cm)
+// phiestribo    - Diâmetro do estribo                                       (mm)
+// d             - Altura útil da peça (0.8.h <= d <= 0.9.h)                 (cm)
+// md            - Momento fletor de cálculo                                 (kN.m)
+// fcd           - Resistência de cálculo à compressão do concreto           (MPa)
+// fyd           - Resistência de cálculo à tração do aço                    (MPa)
+// dlinha        - Altura útil complementar da peça                          (cm)
+// ac            - Área da seção transversal                                 (cm2)
+// inercia       - Momento de inércia                                        (cm4)
+// ycg           - Baricentro da peça sem armadura no estádio I              (cm)
+// w             - Módulo resistente da peça sem armadura no estádio         (cm3)
 // lambda        - Redutor da altura y do diagrama de tensões do concreto
 // alfac         - Redutor da tensão (sigma_cd) do diagrama de tensões do concreto
 // epsiloncu     - Deformação máximo suportada pelo concreto na compressão
 // duct          - Coeficiente de ductilidade NBR 6118 (item 14.6.4.3)
-// fctm          - Resistência de cálculo à tração do concreto no ensaio de comp. diametral  (MPa)
-// fctksup       - Resistência de cálculo à tração do concreto no ensaio de tração na flexão (MPa)
 // kmd           - Momento de cálculo (md) admensional
 // kx            - Linha neutra (x) admensional
 // kz            - Braço de alavanca (z) admensional
 // x e xx        - Linha neutra  por dois modelso de cálculo           (cm)
 // num1 e num2   - Verificadores da seção para o momento informado
-// epsilonsyield - Deformação de escoamento do aço escolhido
+// epsilonu      - Deformação de escoamento do aço de protenção
 // lim23         - Fronteira do domínio 2 com o 3
 // lnlim23       - Limite de linha neutra para D2 com D3
 // lim34         - Fronteira do domínio 3 com o 4
@@ -102,9 +97,9 @@
 
 // ---------- Chamada de funções
 //
-include_once('FLEXAOPURACONCRETOARMADO01_v00_erros.php');
-include_once('FLEXAOPURACONCRETOARMADO01_v00_avisos.php');
-include_once('FLEXAOPURACONCRETOARMADO01_v00_detalhamento.php');
+include_once('FLEXAOPURACONCRETOPROTENDIDO01_v00_erros.php');
+include_once('FLEXAOPURACONCRETOPROTENDIDO01_v00_avisos.php');
+include_once('FLEXAOPURACONCRETOPROTENDIDO01_v00_detalhamento.php');
 //
 // ----------
 
@@ -117,68 +112,79 @@ include_once('FLEXAOPURACONCRETOARMADO01_v00_detalhamento.php');
 //
 // print setup
 echo "-----------CORETEC TOOLS - CONCRETE-----------\n";
-echo "CONCRETO ARMADO - DIMENSIONAMENTO DE SEÇÃO RETANGULARES SOB AÇÃO DE UM MOMENTO FLETOR\n";
+echo "CONCRETO PROTENDIDO - DIMENSIONAMENTO DE SEÇÃO RETANGULARES SOB AÇÃO DE UM MOMENTO FLETOR\n";
 echo "-----------------------------------------------\n\n";
 
 // Step 1.1: Parâmetros de entrada do algoritmo
 $fck        = 20;
 $dmax       = 1.9;
-$fyk        = 500;
+$fpyk       = 1680;
+$fptk       = 1900;
 $bw         = 25;
 $h          = 90;
 $cob        = 3.0;
 $phiestribo = 6.3;
 $d          = 80;
 $md         = 600;
+$epsilonu   = 0.035;
+$Ep         = 110000;
+$sigmapdinf = 1000;
 
 // Step 1.2: Verificação de erros na fase de dados de entrada
 if ($fck > 90) {
     $erro = 1;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($cob > 5.5) {
     $erro = 2;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($phiestribo > 16) {
     $erro = 3;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
-if ($fyk > 600) {
+if ($fpyk > 1900) {
     $erro = 4;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($fck <= 0 || $fyk <= 0 || $bw <= 0 || $h <= 0 || $cob <= 0 || $phiestribo <= 0 || $d <= 0 || $md <= 0 || $dmax <=0) {
     $erro = 5;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($fck > $fyk) {
     $erro = 6;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($d >= $h) {
     $erro = 7;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
 
 if ($dmax > 10) {
     $erro = 8;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
+
+if ($sigmapdinf>$fpyd {
+    $erro = 9;
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
+    return;
+}
+
 
 // print setup
 echo "-----------------------------------------------\n";
@@ -186,7 +192,7 @@ echo "PARÂMETROS DE ENTRADA:\n";
 echo "-----------------------------------------------\n";
 echo "fck = $fck MPa\n";
 echo "DMC = $dmax cm\n";
-echo "fyk = $fyk MPa\n";
+echo "fpyk = $fpyk MPa\n";
 echo "bw  = $bw cm\n";
 echo "h   = $h cm\n";
 echo "cob = $cob cm\n";
@@ -202,8 +208,7 @@ echo "-----------------------------------------------\n\n";
 //
 //
 // Step 2.1: Determinação da resistência de cálculo para o concreto e aço
-$fcd = $fck / 1.4;
-$fyd = $fyk / 1.15;
+$fcd  = $fck / 1.4;
 
 // Step 2.2: Determinação altura dlinha
 $dlinha = $h - $d;
@@ -230,31 +235,23 @@ if ($fck <= 50) {
 }
 $fctksup     = 1.3*$fctm;
 
-// Step 2.5: Deformação de escoamento do aço a ser considerada
-if ($fyk == 250) {
-    $epsilonsyield = 1.04/1000;
-} elseif ($fyk == 500) {
-    $epsilonsyield = 2.07/1000;
-} else {
-    $epsilonsyield = 2.48/1000;
-}
-
 echo "-----------------------------------------------\n";
 echo "PROCESSAMENTO DAS PROPRIEDADES DO MATERIAL E SISTEMA:\n";
 echo "-----------------------------------------------\n";
-echo "lambda         = $lambda\n";
-echo "alfac          = $alfac\n";
-echo "epsiloncu      = $epsiloncu\n";
-echo "epsilonsescoa  = $epsilonsyield\n";
-echo "x/d máximo     = $duct\n";
-echo "fcd            = $fcd MPa\n";
-echo "fyd            = $fyd MPa\n";
-echo "fctm           = $fctm MPa\n";
-echo "dlinha inicial = $d cm\n";
-echo "Área concreto  = $ac cm2\n";
-echo "ycg seção      = $ycg cm\n";
-echo "Inércia        = $inercia cm4\n";
-echo "Módulo W       = $w cm3\n";
+echo "lambda                 = $lambda\n";
+echo "alfac                  = $alfac\n";
+echo "Módulo de elasticidade = $Ep\n";
+echo "Tensão de protensão    = $sigmapdinf\n";
+echo "epsiloncu              = $epsiloncu\n";
+echo "epsilonsescoa          = $epsilonu\n";
+echo "x/d máximo             = $duct\n";
+echo "fcd                    = $fcd MPa\n";
+echo "fpyd                   = $fpyd MPa\n";
+echo "dlinha inicial         = $d cm\n";
+echo "Área concreto          = $ac cm2\n";
+echo "ycg seção              = $ycg cm\n";
+echo "Inércia                = $inercia cm4\n";
+echo "Módulo W               = $w cm3\n";
 echo "-----------------------------------------------\n\n";
 //
 //
@@ -263,9 +260,9 @@ echo "-----------------------------------------------\n\n";
 //=========================================================================%
 //
 //
-// Step 3.1: Dimensionamento por formulações admiensionais Capítulo 3 --> CARVALHO, R.C.; PINHEIRO, L.M. Cálculo e Detalhamento de Estruturas Usuais de Concreto Armado, v2. 1.ed. São Paulo: Editora PINI Ltda., 2009. 589p.
+// Step 3.1: Dimensionamento por formulações adimensionais Capítulo 3 --> CARVALHO, R.C.; PINHEIRO, L.M. Cálculo e Detalhamento de Estruturas Usuais de Concreto Armado, v2. 1.ed. São Paulo: Editora PINI Ltda., 2009. 589p.
 
-// Step 3.1.1: Fatores admiensionais e linha neutra da seção
+// Step 3.1.1: Fatores adimensionais e linha neutra da seção
 $kmd  = $md * 100 / ($bw * pow($d, 2)* ($fcd/10));
 
 // Step 3.1.2: Trechos da equação de linha neutra (x) na forma geral
@@ -277,14 +274,35 @@ $kx   = (1 - sqrt(1-(2*$kmd)/$alfac))/$lambda;
 
 // Step 3.1.4: Verificação de erros na fase de dimensionamento
 if (is_nan($kx)) {
-    $erro = 9;
-    FLEXAOPURACONCRETOARMADO01_v00_erros($erro);
+    $erro = 10;
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_erros($erro);
     return;
 }
+
+
+
 
 $kz   = (1-0.5*$lambda*$kx);
 $xx   = ($d-sqrt($num2-$num1))/$lambda;
 $x    = $kx * $d;
+
+// Alongamento do aço no ELU                                // epsilons     - Alongamento do aço no ELU
+$epsilons = ($epsiloncu * $d / $x) - $epsiloncu;
+//
+// Alongamento do aço na protenção                          // epsilonalong - Alongamento do aço na protenção
+$epsilonalong = $sigmapdinf / $Ep;                             // sigmapdinf   - Tensão de protenção no tempo infinito (Mpa)
+//                                                          // Ep           - Módulo de elasticidade do aço (MPa)
+// Alongamento total
+$epsilonts = $epsilons + $episilonalong;                    // epsilonts    - Alongamento total
+//
+// Tensão para alongamento quando sigampd maior que fpyd
+$sigmapd = $fpyd + (($fpd - $fpyd)/($epsilonu - $epsilons)) * ($epsilonts - $epsilons);
+
+//                                                           // fpd      - Tensão mínima de ruptura
+//                                                           // fpyd     - Tensão mínima a 1% de deformação 
+//                                                           // epsilonu - alongamento sobre carga 
+
+
 
 // Step 3.2: Checando o domínio da peça
 // Step 3.2.2: Determinação do limite entre domínio 2 e 3
@@ -292,7 +310,7 @@ $lim23   = $epsiloncu / ($epsiloncu + 0.01);
 $lnlim23 = $lim23*$d;
 
 // Step 3.2.3: Determinação do limite entre domínio 3 e 4
-$lim34   = $epsiloncu / ($epsiloncu + $epsilonsyield);
+$lim34   = $epsiloncu / ($epsiloncu + $epsilonu);
 $lnlim34 = $lim34*$d;
 
 //  Step 3.2.4: Escolhendo o modelo de dimensionamento (pode ser armadura simples ou dupla)
@@ -309,7 +327,7 @@ if ($kx < $lim23) {
     $dominio  = "Domínio 3";
     $armadura = "DUPLA";
     $aviso = 1;
-    FLEXAOPURACONCRETOARMADO01_v00_avisos($aviso);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_avisos($aviso);
 }
 
 echo "-----------------------------------------------\n";
@@ -388,7 +406,7 @@ $astot  = $as1 + $as2;
 // Step 3.6.3: Verificação da armadura máxima da seção
 if ($astot > $asmax) {
     $aviso = 2;
-    FLEXAOPURACONCRETOARMADO01_v00_avisos($aviso);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_avisos($aviso);
 }
 
 // Step 3.7: Armadura de pele na seção
@@ -445,10 +463,10 @@ echo "DETALHAMENTO PARA UMA SITUAÇÃO DE ARMADURA DUPLA\n";
 echo "-----------------------------------------------\n";
 
 echo "\nBARRAS TRACIONADAS TIPO AS1\n";
-    FLEXAOPURACONCRETOARMADO01_v00_detalhamento($dmax, $as1, $h, $d, $bw, $cob, $phiestribo);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_detalhamento($dmax, $as1, $h, $d, $bw, $cob, $phiestribo);
 
 echo "\nBARRAS COMPRIMIDAS TIPO AS2\n";
-    FLEXAOPURACONCRETOARMADO01_v00_detalhamento($dmax, $as2, $h, $d, $bw, $cob, $phiestribo);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_detalhamento($dmax, $as2, $h, $d, $bw, $cob, $phiestribo);
 
 } elseif ($armadura == "SIMPLES") {
 
@@ -458,7 +476,7 @@ echo "DETALHAMENTO PARA UMA SITUAÇÃO DE ARMADURA SIMPLES\n";
 echo "-----------------------------------------------\n";
 
 echo "\nBARRAS TRACIONADAS TIPO AS1\n";
-    FLEXAOPURACONCRETOARMADO01_v00_detalhamento($dmax, $as1, $h, $d, $bw, $cob, $phiestribo);
+    FLEXAOPURACONCRETOPROTENDIDO01_v00_detalhamento($dmax, $as1, $h, $d, $bw, $cob, $phiestribo);
     
 }
 ?>
